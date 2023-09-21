d3306161e45974b0b811b9f0632a1a5ee0d58c10
sparsh-kadian <sparshkadian4@gmail.com>
2023-09-21 22:22:59 +0530

    added assets
    adding the all the files required for the code

---
+++ src/components/Tours.js
@@ -0,0 +1,19 @@
+import Card from "./Card";
+function Tours({tours, removeTour}) {
+    return(
+        <div className="container">
+            <div>
+                <h2 className="title">Plan With Me</h2>
+            </div>
+            <div className="cards">
+                {
+                    tours.map( (tour) => {
+                        return <Card {...tour} key={tour.id} removeTour={removeTour}></Card>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Tours;
